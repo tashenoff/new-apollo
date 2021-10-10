e6c8cfd233306c2fcae01b7795577a49685c4715
mocliamg4991 <apollo-admin@656145-cm15582.tmweb.ru>
2021-10-10 23:18:24 +0300

    server

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -1,6 +1,6 @@
-command = '/home/apollo-admin/apollo-site/apollo-venv/bin/activate'
+command = '/home/apollo-admin/apollo-site/apollo-venv/bin/gunicorn'
 pythonpath = '/home/apollo-admin/apollo-site/new-apollo/'
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:8001'
 workers = 3
 user = 'apollo-admin'
 limit_request_fields = 32000
