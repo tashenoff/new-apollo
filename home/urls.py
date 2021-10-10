eb6d4f812dc1d19f75ce65dc55fb038aceb17e39
mocliamg4991 <clinkz1994@gmail.com>
2021-10-10 23:18:06 +0600

    prod_deploy

--- home/urls.py
+++ home/urls.py
@@ -8,7 +8,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/all/', views.RecordListView.as_view()),
-    path('/record/create/', views.RecordCreateView.as_view()),
-    path('/record/detail/<int:pk>/', views.RecordDetailView.as_view()),
+    path('all/', views.RecordListView.as_view()),
+    path('record/create/', views.RecordCreateView.as_view()),
+    path('record/detail/<int:pk>/', views.RecordDetailView.as_view()),
 ]
