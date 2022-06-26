a94d32a820aec49f612bb1f1603586781b8f5321
mocliamg4991 <60917945+mocliamg4991@users.noreply.github.com>
2022-06-26 15:10:47 +0600

    vidopro add to allowedhost

--- acrm/prod_settings.py
+++ acrm/prod_settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-c7*#6@p6+p^eSDArfoblh!^nledk!bwy4f@f5x%08%kw+o@n2'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'apollo.kz', 'www.apollo.kz']
+ALLOWED_HOSTS = ['127.0.0.1', 'apollo.kz', 'www.apollo.kz','vidopro.ru','www.vidopro.ru']
 
 
 
